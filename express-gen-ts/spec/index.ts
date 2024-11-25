83e3347e2e6b0dc896125f45e4f037073d1c14db
TadeoLu <101262571+TadeoLu@users.noreply.github.com>
2024-11-25 03:31:23 -0300

    Update index.ts

--- express-gen-ts/spec/index.ts
+++ express-gen-ts/spec/index.ts
@@ -37,7 +37,7 @@ const args = parse<IArgs>({
 
 // Init Jasmine
 const jasmine = new Jasmine();
-jasmine.exitOnCompletion = false;
+jasmine.exitOnCompletion = true;
 
 // Set location of test files
 jasmine.loadConfig({
